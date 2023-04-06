ee5d7387eb6ec207bf53279a596f2194ee1db2a5
BranBayou <berhan.baye@gmail.com>
2023-04-06 16:35:51 +0300

    Add functions to add to-do lists

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import _ from 'lodash';
 import './style.css';
-function component() {
-  const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
+const todoList = document.getElementById('todo-lists');
+
+const listArray = [
+  {
+    description: 'wash the dishes',
+    completed: true,
+    index: 1,
+  }, {
+    description: 'complete To Do list project',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'wash my car',
+    completed: true,
+    index: 3,
+  },
+];
+const showTasks = (task) => {
+  listArray.sort((a, b) => a.index - b.index);
+  todoList.innerHTML += `<div class="todo" id="todo-con">
+                          <div class="left-side">
+                            <input type="checkbox" id="todo" name="todo" value="">
+                            <p>${listArray[task].description}</p>
+                          </div>
+                          <div class="right-side">
+                            <i class="fa-solid fa-ellipsis-vertical"></i>
+                            <i class="fa-regular fa-trash-can trash-can"></i>
+                          </div>
+                        </div>`;
+};
+for (let i = 0; i < listArray.length; i += 1) {
+  showTasks(i);
 }
-
-document.body.appendChild(component());
