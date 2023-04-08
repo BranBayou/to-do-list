f3897eb83766fb6e37ab737309252e09b84c4b5c
BranBayou <berhan.baye@gmail.com>
2023-04-09 00:44:22 +0300

    update index.js file

--- src/index.js
+++ src/index.js
@@ -1,106 +1,37 @@
 import './style.css';
+import {
+  renderTodo, todoAdder, dragger, todoFilter,
+} from './modules/renderer.js';
 
-const form = document.getElementById('todoform');
-const todoInput = document.getElementById('newtodo');
-const todosListEl = document.getElementById('todos-list');
-let todos = [];
-const toLocalStorage = () => {
-  localStorage.setItem('todos', JSON.stringify(todos));
-};
+let todoList = [];
 
-const renderTodos = () => {
-  todosListEl.innerHTML = '';
-  if (todos) {
-    todos.forEach((todo, index) => {
-      todosListEl.innerHTML += `
-      <div class="todo" id="${index}">
-        <div class="left-side">
-          <input type="checkbox" id="checkbox" class="checkbox">
-          <p class="task-input-field">${todo.description}</p>
-        </div>
-        <div class="right-side">
-          <i class="bi bi-three-dots-vertical edit"></i>
-          <i class="bi bi-trash delete"></i>
-        </div>
-      </div>
-      `;
-    });
+const addTodo = document.getElementById('add-btn');
+const textInput = document.getElementById('todo-input');
+// clear selected button
+const clearSelected = document.querySelector('.clearChecked');
+// reset button
+const refresh = document.querySelector('.fresher');
 
-    // Function to handle edit action
-    const handleEdit = (index) => {
-      // Get the task description element
-      const taskDescEl = document.getElementsByClassName('task-input-field')[index];
-      // Replace the task description element with an input field
-      const newInputEl = document.createElement('input');
-      newInputEl.setAttribute('type', 'text');
-      newInputEl.setAttribute('value', taskDescEl.innerText);
-      newInputEl.classList.add('edit-input');
-      taskDescEl.replaceWith(newInputEl);
-      // Focus on the input field
-      newInputEl.focus();
-      // Add event listener to the input field to handle the edit action
-      newInputEl.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-          // Save the edited todo
-          todos[index].description = newInputEl.value;
-          toLocalStorage();
-          renderTodos();
-        }
-      });
-    };
-
-    // Add event listener to each edit icon
-    const editIcon = document.getElementsByClassName('edit');
-    for (let i = 0; i < editIcon.length; i += 1) {
-      editIcon[i].addEventListener('click', () => {
-        handleEdit(i);
-      });
-    }
-    // Function to handle delete action
-    const handleDelete = (index) => {
-      todos.splice(index, 1);
-      for (let i = 0; i < todos.length; i += 1) {
-        todos[i].index = i + 1;
-      }
-      toLocalStorage();
-      renderTodos();
-    };
-    // Add event listener to each delete icon
-    const deleteIcon = document.getElementsByClassName('delete');
-    for (let i = 0; i < deleteIcon.length; i += 1) {
-      deleteIcon[i].addEventListener('click', () => {
-        handleDelete(i);
-      });
-    }
+textInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    todoAdder(todoList, textInput, renderTodo, clearSelected);
   }
-};
-
-const saveTodo = () => {
-  const todoValue = todoInput.value;
-  const isEmpty = todoValue === '';
-  if (isEmpty) {
-    todoInput.innerHTML = 'Empty todo';
-  } else {
-    const todo = {
-      description: todoValue,
-      completed: false,
-      index: todos.length + 1,
-    };
-    todos.push(todo);
-    todoInput.value = '';
-    toLocalStorage();
-    renderTodos();
-  }
-};
-
-form.addEventListener('submit', (event) => {
+});
+addTodo.addEventListener('click', (event) => {
   event.preventDefault();
-  saveTodo();
+  todoAdder(todoList, textInput, renderTodo, clearSelected);
 });
 
-window.addEventListener('load', () => {
-  if (localStorage.getItem('todos')) {
-    todos = JSON.parse(localStorage.getItem('todos'));
-  }
-  renderTodos();
+clearSelected.addEventListener('click', () => {
+  todoList = todoList.filter((element) => element.completed !== true);
+  renderTodo(todoList, clearSelected, dragger, todoFilter);
+});
+
+refresh.addEventListener('click', () => {
+  todoList = [];
+  renderTodo(todoList, clearSelected, dragger, todoFilter);
+});
+window.addEventListener('DOMContentLoaded', () => {
+  renderTodo(todoList, clearSelected, dragger, todoFilter);
 });
